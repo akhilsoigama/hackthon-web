9a890e884b790e28aa92f2df6ef799f6df725c6f
akhil Soigama <akhilsoigama@gmail.com>
2025-09-19 16:43:12 +0530

    redirect issue

--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import Navbaar from '../section/Navbaar';
+import Sidebar from '../section/Sidebar';
 
+// Dashboard Sections
 import Progress from '../section/Progress';
-// import Assignments from '../section/Assignment';
 import Calendar from '../section/Calendar';
 import ChatBot from '../section/ChatBot';
 import Settings from '../section/Settings';
-import Overview from '../section/overview';
 
 // Nabha Management
 import InstituteCreate from '../section/Nabha-management/institute-management/Institute-create';
@@ -14,61 +16,67 @@ import GovtServeyCreate from '../section/Nabha-management/servey-master/Govt-Ser
 import GovtServeyList from '../section/Nabha-management/servey-master/Govt-Servey-List';
 import RolePermissionCreate from '../section/Nabha-management/Role-permission/Role-Permission-Create';
 import RolePermissionList from '../section/Nabha-management/Role-permission/Role-Permission-List';
-import AssignmentCreate from '../section/Student-management/Assignment-master/assignment-create';
-import AssignmentList from '../section/Student-management/Assignment-master/assignment-list';
-import LessonCreate from '../section/Student-management/Lession-master/lession-create';
-import LessonList from '../section/Student-management/Lession-master/lession-list';
-import QuizCreate from '../section/Student-management/Quiz-master/quiz-create';
-import QuizList from '../section/Student-management/Quiz-master/quiz-list';
-import LeaveCreate from '../section/Leave-management/Leave-master/leave-create';
-import LeaveList from '../section/Leave-management/Leave-master/leave-list';
-import LeaveApprovalDone from '../section/Leave-management/Leave-Approval-master/leave-approval-done';
-import AssignmentUploadCreate from '../section/Student-upload/Assignment-upload/assignment-upload-create';
-import AssignmentUploadList from '../section/Student-upload/Assignment-upload/assignment-upload-list';
-import LessionUploadCreate from '../section/Student-upload/Lession-upload/lession-upload-create';
-import LessonUploadList from '../section/Student-upload/Lession-upload/lession-upload-list';
-import Quiz from '../section/quiz';
 
-// Institute management
+// Institute Management
 import FacultyCreate from '../section/Institute-management/faculty/faculty-create';
 import FacultyList from '../section/Institute-management/faculty/faculty-list';
 import StudentCreate from '../section/Institute-management/students/student-create';
 import StudentList from '../section/Institute-management/students/student-list';
 import InstituteSurveyCreate from '../section/Institute-management/institure-serveys/servey-create';
 import InstituteSurveyList from '../section/Institute-management/institure-serveys/servey-list';
-import { useState } from 'react';
-import Navbaar from '../section/Navbaar';
-import Sidebar from '../section/Sidebar';
 
+// Student Management
+import AssignmentCreate from '../section/Student-management/Assignment-master/assignment-create';
+import AssignmentList from '../section/Student-management/Assignment-master/assignment-list';
+import LessonCreate from '../section/Student-management/Lession-master/lession-create';
+import LessonList from '../section/Student-management/Lession-master/lession-list';
+import QuizCreate from '../section/Student-management/Quiz-master/quiz-create';
+import QuizList from '../section/Student-management/Quiz-master/quiz-list';
+
+// Leave Management
+import LeaveCreate from '../section/Leave-management/Leave-master/leave-create';
+import LeaveList from '../section/Leave-management/Leave-master/leave-list';
+import LeaveApprovalDone from '../section/Leave-management/Leave-Approval-master/leave-approval-done';
+
+// Student Upload
+import AssignmentUploadCreate from '../section/Student-upload/Assignment-upload/assignment-upload-create';
+import AssignmentUploadList from '../section/Student-upload/Assignment-upload/assignment-upload-list';
+import LessionUploadCreate from '../section/Student-upload/Lession-upload/lession-upload-create';
+import LessonUploadList from '../section/Student-upload/Lession-upload/lession-upload-list';
+
+// Quiz
+import Quiz from '../section/quiz';
+import Overview from '../section/overview';
 
 export default function Routers() {
   const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
 
-const toggleMobileSidebar = () => {
-  setIsMobileSidebarOpen(prev => !prev);
-};
+  const toggleMobileSidebar = () => {
+    setIsMobileSidebarOpen((prev) => !prev);
+  };
+
   return (
-    <BrowserRouter >
+    <BrowserRouter>
       <Routes>
+        {/* Redirect root path to /dashboard */}
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route
           path="/dashboard"
           element={
-             <div className="flex min-h-screen w-full">
-              <Sidebar
-                isMobileOpen={isMobileSidebarOpen}
-                toggleMobileSidebar={toggleMobileSidebar}
-              />
+            <div className="flex min-h-screen w-full">
+              <Sidebar isMobileOpen={isMobileSidebarOpen} toggleMobileSidebar={toggleMobileSidebar} />
               <Navbaar toggleMobileSidebar={toggleMobileSidebar} />
             </div>
           }
         >
           {/* Dashboard Routes */}
+          <Route index element={<Overview />} />
           <Route path="overview" element={<Overview />} />
           <Route path="progress" element={<Progress />} />
           <Route path="events" element={<Calendar />} />
           <Route path="quize" element={<Quiz />} />
-          {/* <Route path="messages" element={<Messages />} /> */}
           <Route path="settings" element={<Settings />} />
+          <Route path="chatbot" element={<ChatBot />} />
 
           {/* Nabha Management Routes */}
           <Route path="nabha-master/institute/create" element={<InstituteCreate />} />
@@ -105,11 +113,7 @@ const toggleMobileSidebar = () => {
           <Route path="student-upload/lession-upload/upload" element={<LessionUploadCreate />} />
           <Route path="student-upload/lession-upload/list" element={<LessonUploadList />} />
 
-          {/* Communication Routes */}
-          <Route path="chatbot" element={<ChatBot />} />
-
-          {/* Default route */}
-          <Route index element={<Overview />} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
         </Route>
       </Routes>
     </BrowserRouter>
